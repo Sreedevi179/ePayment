1d4df6e086a9bdba77bea21422b9fedde16caa28
TuralSuleymani <suleymani.2ral@gmail.com>
2018-10-25 21:14:42 +0400

    initial project

---
+++ js/config/baseConfig.js
@@ -0,0 +1,49 @@
+let baseConfig = {
+    "azeFlag": "img/az.png",
+    "rusFlag": "img/rus.png",
+    "engFlag": "img/eng.png",
+    "topProviders" : {
+                "azercell": {
+                    name: "Azercell",
+                    order: 5,
+                    bgColor: "#ae3456",
+                    image: "img/topProviders/azercell_log.png"
+                },
+                "alfanet": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#ae3fe6",
+                    image: "img/topProviders/azerfon.png"
+                },
+                "alfanet1": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#235089",
+                    image: "img/topProviders/azeriqaz.png"
+                },
+                "alfanet2": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#233289",
+                    image: "img/topProviders/bakielektriksebeke.png"
+                },
+                "alfanet4": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#235089",
+                    image: "img/topProviders/azersu-logo.png"
+                },
+                "alfanet5": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#233289",
+                    image: "img/topProviders/alfanetpen.png"
+                },
+                "alfanet7": {
+                    name: "Alfanet",
+                    order: 1,
+                    bgColor: "#833289",
+                    image: "img/topProviders/alfanetpen.png"
+                }
+    }
+}
